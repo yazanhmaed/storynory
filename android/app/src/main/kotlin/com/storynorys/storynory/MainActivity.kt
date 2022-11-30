4be9b37859433d59cb62bf71b708386cb637126b
yazanhmaed <yhmaed58@gmail.com>
2022-11-30 04:01:50 +0300

    First commit

--- android/app/src/main/kotlin/com/storynorys/storynory/MainActivity.kt
+++ android/app/src/main/kotlin/com/storynorys/storynory/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.storynory.storynory
+package com.storynorys.storynory
 
 import android.os.Bundle
 import com.google.firebase.FirebaseApp
--- lib/models/ads/ad_helper.dart
+++ lib/models/ads/ad_helper.dart
@@ -8,13 +8,13 @@ class AdHelper {
       return 'ee669f08-12fe-4ee5-a521-dd5904fbcce5';
     }
     
-    return 'ca-app-pub-8887736687313297/2361886259';
+    return 'ca-app-pub-8887736687313297/6776199983';
   }
   static String get viewAd {
     if (_testMode) {
       return 'ee669f08-12fe-4ee5-a521-dd5904fbcce5';
     }
     
-    return 'ca-app-pub-8887736687313297/3098013737';
+    return 'ca-app-pub-8887736687313297/8397237562';
   }
 }
